731c00d8cfd92e183eb1fb45d3a5448db2455101
xtedor <santixcolmen2@gmail.com>
2021-04-27 20:49:39 -0500

    segundo1

--- PetsPasto/urls.py
+++ PetsPasto/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Afiliados/', include ('Afiliados.urls')),
+    path('Mascotas/', include ('Mascotas.urls')),
+    path('TiendaMascotas/', include ('TiendaMascotas.urls')),
 ]
